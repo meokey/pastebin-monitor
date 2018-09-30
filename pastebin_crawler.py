b8a0f600dbc2f10520f0d54ff64e05dec1c7e112
Bill <bill@dell>
2018-09-30 15:48:51 -0400

    restrict delayfactor to minimal 0.5

--- pastebin_crawler.py
+++ pastebin_crawler.py
@@ -319,7 +319,7 @@ class Crawler:
                             Logger().log('Good job! You caught up all new pastes since last update! {:d} pastes are already checked'.format(numofpastes-chkedpaste), True)
                             self.delayfactor = self.delayfactor + 0.02*(numofpastes-chkedpaste)
                         else:
-                            self.delayfactor = self.delayfactor - 0.08
+                            self.delayfactor = self.delayfactor - 0.08 if self.delayfactor > 0.5 else 0.5
                     count += 1
 
                 if count == flush_after_x_refreshes:
