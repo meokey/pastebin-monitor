4360bb130b7ad998be6c51be454f584f5c8b57bc
Bill <bill@dell>
2018-10-03 16:04:29 -0400

    fine tunes; more stats

--- pastebin_crawler.py
+++ pastebin_crawler.py
@@ -115,7 +115,7 @@ def all_python_encodings():
 
 
 class Logger:
-
+    verbose = False
     shell_mod = {
         '':'',
        'PURPLE' : '\033[95m',
@@ -129,6 +129,8 @@ class Logger:
        'UNDERLINE' : '\033[4m',
        'RESET' : '\033[0m'
     }
+    def __init__(self,verbose=False):
+        self.verbose = verbose
 
     def log ( self, message, is_bold=False, color='', log_time=True):
         prefix = ''
@@ -146,18 +148,26 @@ class Logger:
             suffix = self.shell_mod['RESET']
 
         message = prefix + message + suffix
-#        print ( message )
-#        sys.stdout.flush()
-## tar log files if the size reachs 256MB
-        size = os.path.getsize(logfile)
-        if size > 1024*1024*256:
+        #print ( message )
+        #sys.stdout.flush()
+
+        if (self.verbose == True) or (is_bold == True):
+## tar log files if the size reachs 128MB
+            size = os.path.getsize(logfile) or 0
+            if size > 1024*1024*128:
                 tarf = logfile+'.gz'
                 mode='w:gz'
                 with tarfile.open(tarf,mode) as out:
                     out.add(logfile)
                 os.remove(logfile)
-        with open(logfile, "a+") as logf:
-          logf.write(message+"\n")
+            with open(logfile, "a+") as logf:
+                logf.write(message+"\n")
+
+    def match(self, err):
+        self.log(err, True, 'CYAN')
+
+    def warn(self, err):
+        self.log(err, True, 'YELLOW')
 
     def error(self, err):
         self.log(err, True, 'RED')
@@ -191,20 +201,20 @@ class Crawler:
                 except KeyboardInterrupt:
                     raise
                 except:
-                    Logger().fatal_error('Malformed regexes file. Format: regex_pattern,URL logging file, directory logging file.')
+                    Logger(self.verbose).fatal_error('Malformed regexes file. Format: regex_pattern,URL logging file, directory logging file.')
 
-#            for regex,file,directory in self.regexes:
-#                Logger ().log ( directory+':\t'+file+':\t'+regex[:68])
-            Logger ().log ( '{:d} regex rules are refreshed.'.format(len(self.regexes)), True)
+            #for regex,file,directory in self.regexes:
+                #Logger (self.verbose).log ( directory+':\t'+file+':\t'+regex[:68])
+            Logger (self.verbose).log ( '{:d} regex rules are refreshed.'.format(len(self.regexes)), True)
 
         except KeyboardInterrupt:
             raise
         except:
-            Logger().fatal_error('{:s} not found or not acessible.'.format(self.REGEXES_FILE))
+            Logger(self.verbose).fatal_error('{:s} not found or not acessible.'.format(self.REGEXES_FILE))
 
 
     def __init__(self):
-#        self.read_regexes()
+        #self.read_regexes()
         self.kill_now = False
         self.delayfactor = 1	# dynamically adjust the delay time of retrieving each paste
         self.min_delayfactor = 0.5	# minimal acceptable delay factor preventing from being banned
@@ -215,9 +225,31 @@ class Crawler:
         self.starttime = time.time()
         self.starttime_ts = get_timestamp()
         self.totalerrors = 0
+## values used in debug mode to track run time stats
+        self.stats = {}
+        self.init_stat('get_pastes')
+        self.init_stat('check_paste')
+
 ## register os signals to response to kill interruption
         signal.signal(signal.SIGINT, self.handle)
         signal.signal(signal.SIGTERM, self.handle)
+
+    def init_stat(self,stat):
+        if stat not in self.stats:
+            self.stats[stat] = {}
+        self.stats[stat]['total'] = 0
+        self.stats[stat]['num'] = 0
+        self.stats[stat]['avg'] = lambda :self.stats[stat]['total']/self.stats[stat]['num'] if self.stats[stat]['num'] != 0 else 0
+    def check_stat(self,start,stat):
+        if stat not in self.stats:
+            return 0,0
+        if start >= 0:
+            now = time.time()
+            self.stats[stat]['num'] += 1
+            self.stats[stat]['total'] += now - start
+            return now - start, (now - start - self.stats[stat]['avg']()) / self.stats[stat]['avg']()
+        else:	# return avg if start==0
+            return self.stats[stat]['avg']()
 
     def runduration(self,timestamp1, timestamp2):
         dt1 = datetime.datetime.fromtimestamp(timestamp1)
@@ -237,13 +269,15 @@ class Crawler:
 
     def conclude(self):
 ## stats from startup
-        Logger ().log ( 'Since started at {:s}, the program has run for {:s}.\nIt processed {:d} pastes, including {:d} recorded and {:d} errors.'.format(self.starttime_ts, self.runduration(self.starttime,time.time()), self.totalpastes, self.validpastes, self.totalerrors), True)
+        Logger(self.verbose).log ('Since started at {:s}, the program has run for {:s}.'.format(self.starttime_ts, self.runduration(self.starttime,time.time())), True)
+        Logger(self.verbose).log ('It processed {:d} pastes, including {:d} recorded and {:d} errors.'.format(self.totalpastes, self.validpastes, self.totalerrors), True)
+        Logger(self.verbose).log ('Averagely it took {:.2f}s to fetch pastes, and {:.2f}s to check a single paste.'.format(self.stats['get_pastes']['avg'](), self.stats['check_paste']['avg']()), True)
 
     def __del__(self):
         self.conclude()
 
     def get_pastes ( self ):
-        Logger ().log ( 'Getting pastes', True )
+        Logger (self.verbose).log ( 'Getting pastes', True )
         try:
             page = PyQuery ( url = self.PASTES_URL )
         except KeyboardInterrupt:
@@ -275,6 +309,7 @@ class Crawler:
             except:
                 pass
             if not worked:
+                Logger(self.verbose).warn('Using UTF-8 to get_pastes does not work, try other encodings...')
                 for enc in all_python_encodings():
                     try:
                         page_html = page.html(encoding=enc)
@@ -297,20 +332,22 @@ class Crawler:
             paste_txt = PyQuery ( url = paste_url )('#paste_code').text()
 
             for regex,file,directory in self.regexes:
+                if self.kill_now == True:
+                    exit()
                 r = re.search ( regex, paste_txt, re.IGNORECASE )
                 if r:
-                    Logger ().log ( 'Found a matching paste: ' + paste_url.rsplit('/')[-1] + ' (' + file + '): '+ r[0], True, 'CYAN' )
+                    Logger ().match( 'Found a matching paste: ' + paste_url.rsplit('/')[-1] + ' (' + file + '): '+ r[0] )
                     self.save_result ( paste_url,paste_id,'data/'+file,'data/'+directory )
                     return True
-#            Logger ().log ( 'Not matching paste: ' + paste_url )
+            #Logger (self.verbose).log ( 'Not matching paste: ' + paste_url )
         except KeyboardInterrupt:
             raise
         except Exception as inst:
             self.totalerrors += 1
             if str(inst) == 'HTTP Error 404: Not Found':
-                Logger ().log ( '404 Error reading paste {:s}.'.format(paste_id), True, 'YELLOW')	# likely being removed
+                Logger ().warn ( '404 Error reading paste {:s}.'.format(paste_id))	# likely being removed
             else:
-                Logger ().log ( 'Error reading paste {:s} (probably encoding issue or regex issue), error is {:s}.'.format(paste_id,str(inst)), True, 'YELLOW')
+                Logger ().warn ( 'Error reading paste {:s} (probably encoding issue or regex issue), error is {:s}.'.format(paste_id,str(inst)))
         return False
 
     def save_result ( self, paste_url, paste_id, file, directory ):
@@ -332,14 +369,19 @@ class Crawler:
             paste.write(paste_txt + '\n')
 
 
-    def start ( self, refresh_time = 200, delay = 5, ban_wait = 30, flush_after_x_refreshes=100, connection_timeout=60 ):
+    def start ( self, refresh_time, delay, ban_wait, flush_after_x_refreshes, connection_timeout, verbose ):
         count = 0
+        self.verbose = verbose
         while True:
             if self.kill_now == True:
                 exit()
 
             self.conclude()
+            start = time.time()
             status,pastes = self.get_pastes ()
+            tooktime,times = self.check_stat(start,'get_pastes')
+            if times > 10:
+                Logger(self.verbose).warn('It took {:.2f}s to get_pastes, which is {:.2f} times of average'.format(tooktime,times))
 
             start_time = time.time()
             if status == self.OK:
@@ -348,7 +390,7 @@ class Crawler:
                 totaldelayed = 0
                 chkedpaste = 0
                 numofpastes = len(pastes) or 0
-                Logger().log('Retreived {:d} pastes, will process using delay factor of {:.2f} ...'.format(numofpastes,self.delayfactor),True)
+                Logger(self.verbose).log('Retreived {:d} pastes, will process using delay factor of {:.2f} ...'.format(numofpastes,self.delayfactor),True)
                 self.read_regexes()
                 for paste in pastes:
                     currpaste += 1
@@ -356,17 +398,24 @@ class Crawler:
                     self.new_checked_ids.append ( paste_id )
                     if paste_id not in self.prev_checked_ids:
                         chkedpaste += 1
+                        start = time.time()
+                        #Logger(self.verbose).log('Start processing paste {:s}'.format(paste_id))
                         self.check_paste ( paste_id )
+                        tooktime,times = self.check_stat(start,'check_paste')
+                        if times >= 20:
+                            Logger(self.verbose).error('{:s} might be a giant paste that took {:.2f}s to check, it is {:.2f} times of average'.format(paste_id,tooktime,times))
+                        elif times > 10:
+                            Logger(self.verbose).warn('{:s} took {:.2f}s to check, which is {:.2f} times of average'.format(paste_id,tooktime,times))
                         delaytime = delay*random.uniform(0.6,1.1)*self.delayfactor
                         totaldelayed += delaytime
-                        Logger().log('Paste {:02d}/{:02d} done; Waiting {:.2f} seconds for next paste ...'.format(currpaste,numofpastes,delaytime), False)
+                        Logger(self.verbose).log('Paste {:02d}/{:02d} done; Waiting {:.2f} seconds for next paste ...'.format(currpaste,numofpastes,delaytime))
                         if currpaste < numofpastes:
                             time.sleep(delaytime)
 
                     if currpaste == numofpastes:
-                        Logger().log('Average/Total waiting time is {:.2f}s/{:.2f}m for the pastes'.format(totaldelayed/numofpastes,totaldelayed/60), False)
+                        Logger(self.verbose).log('Average/Total waiting time is {:.2f}s/{:.2f}m for the pastes'.format(totaldelayed/numofpastes,totaldelayed/60), True)
                         if chkedpaste < numofpastes:
-                            Logger().log('Good job! You caught up all new pastes since last update! {:d} pastes are already checked'.format(numofpastes-chkedpaste), True)
+                            Logger(self.verbose).log('Good job! You caught up all new pastes since last update! {:d} pastes are already checked'.format(numofpastes-chkedpaste), True)
                             self.delayfactor = self.max_delayfactor if self.delayfactor >= self.max_delayfactor else (self.delayfactor + 0.04*fabs(numofpastes-chkedpaste))	# slow down a little bit
                         else:
                             self.delayfactor = self.min_delayfactor if self.delayfactor <= self.min_delayfactor else (self.delayfactor - 0.24)	# speed up a little bit
@@ -384,25 +433,25 @@ class Crawler:
                 elapsed_time = time.time() - start_time
                 sleep_time = ceil(max(0,(refresh_time*random.gauss(1,0.2) - elapsed_time)))
                 if sleep_time > 0:
-                    Logger().log('Waiting {:d} seconds to refresh...'.format(sleep_time), True)
+                    Logger(self.verbose).log('Waiting {:d} seconds to refresh...'.format(sleep_time), True)
                     time.sleep ( sleep_time )
                 else:
-                    Logger().log('refresh_time={:d}, elapsed_time={:.2f}, sleep_time={:.2f}'.format(refresh_time,elapsed_time,sleep_time), False)
+                    Logger(self.verbose).log('refresh_time={:d}, elapsed_time={:.2f}, sleep_time={:.2f}'.format(refresh_time,elapsed_time,sleep_time), False)
             elif status == self.ACCESS_DENIED:
                 self.totalerrors += 1
                 delayed += 1
                 self.delayfactor = 1
-                Logger ().log ( 'Damn! It looks like you have been banned (probably temporarily)', True, 'YELLOW' )
+                Logger ().warn ( 'Damn! It looks like you have been banned (probably temporarily)' )
                 for n in range ( 0, ceil(ban_wait*random.gauss(1+delayed*0.2,0.2)) ):
-                    Logger ().log ( 'Please wait ' + str ( ban_wait - n ) + ' more minute' + ( 's' if ( ban_wait - n ) > 1 else '' ) )
+                    Logger (self.verbose).log ( 'Please wait ' + str ( ban_wait - n ) + ' more minute' + ( 's' if ( ban_wait - n ) > 1 else '' ) )
                     time.sleep ( 60 )
             elif status == self.CONNECTION_FAIL:
                 self.totalerrors += 1
-                Logger().log ( 'Connection down. Waiting {:d} seconds and trying again'.format(connection_timeout), True, 'RED')
+                Logger().error ( 'Connection down. Waiting {:d} seconds and trying again'.format(connection_timeout) )
                 time.sleep(connection_timeout)
             elif status == self.OTHER_ERROR:
                 self.totalerrors += 1
-                Logger().log('Unknown error. Maybe an encoding problem? Trying again.'.format(connection_timeout), True,'RED')
+                Logger().error('Unknown error. Maybe an encoding problem? Trying again.'.format(connection_timeout))
                 time.sleep(1)
 
 def parse_input():
@@ -412,11 +461,16 @@ def parse_input():
     parser.add_option('-b', '--ban-wait-time', help='Set the ban wait time (default: 30)', dest='ban_wait', type='int', default=30)
     parser.add_option('-f', '--flush-after-x-refreshes', help='Set the number of refreshes after which memory is flushed (default: 100)', dest='flush_after_x_refreshes', type='int', default=100)
     parser.add_option('-c', '--connection-timeout', help='Set the connection timeout waiting time (default: 60)', dest='connection_timeout', type='float', default=60)
+    parser.add_option('-V', '--verbose', help='enable debug mode for verbose output',dest='verbose', action="store_true")
     (options, args) = parser.parse_args()
-    return options.refresh_time, options.delay, options.ban_wait, options.flush_after_x_refreshes, options.connection_timeout
+    return options.refresh_time, options.delay, options.ban_wait, options.flush_after_x_refreshes, options.connection_timeout, options.verbose
 
 
-try:
-    Crawler ().start (*parse_input())
-except KeyboardInterrupt:
-    Logger ().log ( 'Bye! Hope you found what you were looking for :)', True )
+if __name__ == "__main__":
+    
+    try:
+#        refresh_time, delay, ban_wait, flush_after_x_refreshes, connection_timeout, verbose = parse_input()
+        Crawler ().start (*parse_input())
+#        Crawler ().start (refresh_time=refresh_time,delay=delay,ban_wait=ban_wait,flush_after_x_refreshes=flush_after_x_refreshes,connection_timeout=connection_timeout,verbose=verbose)
+    except KeyboardInterrupt:
+        Logger (self.verbose).log ( 'Bye! Hope you found what you were looking for :)', True )
